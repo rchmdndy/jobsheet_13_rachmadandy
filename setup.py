a97e21de3539d7f2a61bf79cdb03123f86e720d1
rachmadandy <rachmadandyms@gmail.com>
2023-12-01 02:29:11 +0700

    create package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='paket_js_13',
+   version='0.1.0',
+   author='Dandy',
+   author_email='dandy.email@example.com',
+   description='Package untuk menyelesaikan jobsheet 13',
+   packages=find_packages(),
+   classifiers=[
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
